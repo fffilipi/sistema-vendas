16642a4a2fd3996aade5a8bfa27690b83bb9f2b6
devfilipisouza <filipi.souza@crmbonus.com>
2025-03-27 02:25:52 -0300

    feat: adicionar relacionamento e propriedades preenchíveis na classe Sale

--- backend/app/Models/Sale.php
+++ backend/app/Models/Sale.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Sale extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['employee_id', 'amount', 'sale_date'];
+
+    // Uma venda pertence a um vendedor
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
 }
