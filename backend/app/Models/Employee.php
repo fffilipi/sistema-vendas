74401c7e1b57f78f1e9b603bcedcc8d7c4fefb68
devfilipisouza <filipi.souza@crmbonus.com>
2025-03-27 02:25:39 -0300

    feat: adicionar relacionamento de vendas e propriedades preenchíveis na classe Employee

--- backend/app/Models/Employee.php
+++ backend/app/Models/Employee.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'email'];
+
+    // Um vendedor pode ter várias vendas
+    public function sales()
+    {
+        return $this->hasMany(Sale::class);
+    }
 }
