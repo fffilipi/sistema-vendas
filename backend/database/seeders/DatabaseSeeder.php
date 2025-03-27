0cae88569636000cc60350638c258b5a64e86172
devfilipisouza <filipi.souza@crmbonus.com>
2025-03-27 02:27:40 -0300

    feat: atualizar DatabaseSeeder para criar 10 vendedores com 5 vendas cada

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -2,22 +2,16 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Employee;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Employee::factory()
+            ->count(10) // Cria 10 vendedores
+            ->hasSales(5) // Cada vendedor tem 5 vendas
+            ->create();
     }
 }
