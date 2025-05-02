dfa3cda4137eabc77bf13f959128557b9ca80913
devfilipisouza <filipi.souza@crmbonus.com>
2025-05-02 01:13:28 -0300

    refactor: Organize binding of repositories in AppServiceProvider for improved readability

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Repositories\Contracts\UserRepositoryInterface;
 use App\Repositories\UserRepository;
+use App\Repositories\Contracts\TokenRepositoryInterface;
+use App\Repositories\TokenRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(
+            UserRepositoryInterface::class,
+            UserRepository::class
+        );
+        
+        $this->app->bind(
+            TokenRepositoryInterface::class,
+            TokenRepository::class
+        );        
     }
 
     /**
